c3817684109504a85d8f122ca488ee5196e1b384
VladuslavFil <vlad.fil.mobile@gmail.com>
2025-09-17 12:08:43 +0300

    finish

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,12 +6,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Vitaliy Kalamuniak</p>
+          <p>Developer: Vladyslav Fil</p>
           <p>
             Contact us:
-            <a href="mailto:vitalikkalamunak@gmail.com">
-              vitalikkalamunak@gmail.com
-            </a>
+            <a href="mailto:student@notehub.app">student@notehub.app</a>
           </p>
         </div>
       </div>
